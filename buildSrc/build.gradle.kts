c2a2a18b33ce481a7d9d93c8fc31070bd9486548
shenzhen2017 <2868405029@qq.com>
2022-06-05 10:48:47 +0800

    ADD: BuildSrc

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,19 @@
+
+plugins {
+    `kotlin-dsl`
+    kotlin("jvm") version "1.6.21"
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(gradleApi())
+    compileOnly("com.android.tools.build:gradle:7.2.0")
+    compileOnly("commons-io:commons-io:2.6")
+    compileOnly("commons-codec:commons-codec:1.15")
+    compileOnly("org.ow2.asm:asm-commons:9.2")
+    compileOnly("org.ow2.asm:asm-tree:9.2")
+}
